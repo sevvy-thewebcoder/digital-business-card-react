7be672bdc3434f7c1dee953c6f08a12e23866005
Webcoder-Sevvy <sevvy__dhiman@outlook.com>
2023-03-13 17:11:49 +0530

    Updated the text content and CSS styles

--- src/components/Main-Content.jsx
+++ src/components/Main-Content.jsx
@@ -7,19 +7,19 @@ export default function MainContent() {
             <section className="about--data-section">
                 <h2 className="section--title">About</h2>
                 <p className="section--description">
-                    I am a frontend developer with a particular 
-                    interest in making things simple and automating 
-                    daily tasks. I try to keep up with security 
-                    and best practices, and am always looking for 
-                    new things to learn.
+                    I am a self-learned Frontend Developer with a 
+                    particular interest in making things simple and 
+                    automating daily tasks. I try to keep up with 
+                    security and best practices, and am always looking 
+                    for new things to learn.
                 </p>
             </section>
             <section className="interests--data-section">
                 <h2 className="section--title">Interests</h2>
                 <p className="section--description">
-                    Food expert. Music scholar. Reader. 
-                    Internet fanatic. Bacon buff. Entrepreneur.
-                    Travel geek. Pop culture ninja. Coffee fanatic.
+                    Coding & Programming. Science & Technology Inventions. 
+                    Folk-Music Lover. Indian Foods. Punjabi Book Reader. 
+                    Detective Movies. Punjabi & Western Fashion. Tea Fanatic.
                 </p>
             </section>
         </main>
